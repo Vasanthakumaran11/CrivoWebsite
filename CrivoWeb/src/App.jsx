2932a7f0802458c56b98c86eeaccc4679c24224d
Vasanthakumaran11 <vasanthakumaran0011@gmail.com>
2026-01-27 23:33:58 +0530

    feat: added NavBar and Home page

--- CrivoWeb/src/App.jsx
+++ CrivoWeb/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import Navbar from './components/navbar'
+import Home from './components/home'
 
 function App() {
   
   return (
     <>
-      <div className="bg-black min-h-screen flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-white">Welcome to CrivoWeb</h1>
-      </div>
+      <Navbar />
+      <Home />
     </>
   )
 }
